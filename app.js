e38afe4d9dc676a294759042bd36c8e08aa537f3
amuuu-ivi <l.amuuivi@gmail.com>
2024-07-29 15:08:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+//alert("bienvenu à tout le monde sauf dark ૮Ꮚྀིᴗ͈ ‸ ᴗ͈꒱১")
+
+//console.log("૮꒰˵• ﻌ •˵꒱ა")
+
+//let name = "manel";
+
+//console.log("cc"+ name);
+//console.log("sdkk" +name+ "?");
+
+let name = prompt("𝒃𝒊𝒆𝒏𝒗𝒆𝒏𝒖 à 𝒕𝒐𝒖𝒕 𝒍𝒆 𝒎𝒐𝒏𝒅𝒆 𝒔𝒂𝒖𝒇 𝒅𝒂𝒓𝒌 ૮Ꮚྀིᴗ͈ ‸ ᴗ͈꒱১ ! 𝒒𝒖'𝒆𝒔𝒕 𝒄𝒆 𝒒𝒖𝒆 𝒕𝒖 𝒗𝒊𝒆𝒏𝒔 𝒇𝒂𝒊𝒓𝒆 𝒊𝒄𝒊 ?");
+
+alert ("coucou" +name);
+
+let couleur = prompt(" 𝒄'𝒆𝒔𝒕 𝒒𝒖𝒐𝒊 𝒕𝒂 𝒄𝒐𝒖𝒍𝒆𝒖𝒓 𝒑𝒓𝒆𝒇𝒆𝒓𝒆𝒆 ? ?꒰˵• ﻌ •˵꒱");
+
+
+let pseudo = prompt("𝒆𝒏𝒔𝒖𝒊𝒕𝒆, 𝒕𝒐𝒏 𝒑𝒔𝒆𝒖𝒅𝒐 ? ?૮₍˶ᵔ ᵕ ᵔ˶ ₎ა");
+
+let age = prompt("𝒆𝒏𝒇𝒊𝒏, 𝒕𝒐𝒏 𝒂𝒈𝒆 ?");
+if (age<3) {
+	alert("𝒕𝒖 𝒆𝒔 𝒕𝒓𝒐𝒑 𝒋𝒆𝒖𝒏𝒆 ! :( 𝒓𝒆𝒗𝒊𝒆𝒏𝒔 𝒅𝒂𝒏𝒔 𝒒𝒖𝒆𝒍𝒒𝒖𝒆𝒔 𝒂𝒏𝒏𝒆𝒆𝒔 𝒋𝒆 𝒔𝒆𝒓𝒂𝒊𝒔 𝒆𝒏𝒄𝒐𝒓𝒆 𝒍à");
+} else {
+	alert("𝒆𝒏𝒄𝒐𝒓𝒆 𝒖𝒏𝒆 𝒅𝒆𝒓𝒏𝒊𝒆𝒓𝒆 𝒄𝒉𝒐𝒔𝒆 𝒆𝒕 𝒋𝒆 𝒕𝒆 𝒍𝒂𝒊𝒔𝒔𝒆 !")
+}
+let mdp = prompt("𝒍𝒆 𝒎𝒅𝒑 𝒔𝒕𝒑 ໒Ꮚ ´ ˕ ` Ꮚ𑇖");
+if (mdp== "pico") {
+	console.log("mon secret est que jaime pas les chiens >_<");
+}else{
+	console.log("mauvais mdp essaie encore tkt :)");
+}
+let blaze = "𝒃𝒐𝒏 𝒗𝒊𝒔𝒊𝒐𝒏𝒏𝒂𝒈𝒆 !"
+let monTitre = document.getElementById("titre-principal");
+monTitre.innerText = "⋆.*ೃ✧"+ blaze;
+console.log("bienvenu "+prenom+" "+nom+"!");
+
+
+
